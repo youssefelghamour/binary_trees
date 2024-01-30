8a609b16cb5e4a97b486a786f5315125a9c9bd98
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-29 19:18:12 -0800

    3. Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to root of the tree
+ * Return: Nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
