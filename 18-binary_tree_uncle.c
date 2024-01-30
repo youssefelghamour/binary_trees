b09ca606075956a4c43fb0a3d121e166fa3efbed
youssef <youssefelghamour@gmail.com>
2024-01-30 07:07:40 -0800

    simplify 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -33,8 +33,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node)
 		return (NULL);
 
-	if (node->parent && binary_tree_sibling(node->parent))
-		return (binary_tree_sibling(node->parent));
-
-	return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
