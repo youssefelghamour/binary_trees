6d323b98fa2933f66a1aaf7e1daf6ca7804da648
youssef <youssefelghamour@gmail.com>
2024-01-30 03:59:40 -0800

    correct 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -82,8 +82,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
+	if (!tree->left && !tree->right)
 		return (1);
 
-	return (0);
+	return (!binary_tree_balance(tree) && binary_tree_is_full(tree));
 }
