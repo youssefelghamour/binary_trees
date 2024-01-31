167a6a32f37ef0e68dd22bdc4dc823842f80560c
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-30 19:14:31 -0800

    19. Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,31 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * LCA - finds the lowest common ancestor of two nodes
- * @root: root node of the tree
- * @node1: first node
- * @node2: second node
- * Return: lowest common ancestor
+ * level - finds the level of a node in the tree
+ * @node: node to return its level in the tree
+ * Return: level of the node
  */
-binary_tree_t *LCA(binary_tree_t *root,
-		   binary_tree_t *node1, binary_tree_t *node2)
+int level(binary_tree_t *node)
 {
-	binary_tree_t *l_lca, *r_lca;
+	int lv = 0;
 
-	if (root == NULL)
-		return (NULL);
-	if (root == node1 || root == node2)
-		return (root);
-	l_lca = LCA(root->left, node1, node2);
-	r_lca = LCA(root->right, node1, node2);
-	if (l_lca == NULL && r_lca == NULL)
-		return (NULL);
-	if (l_lca && r_lca)
-		return (root);
-	if (l_lca == NULL)
-		return (r_lca);
-	else
-		return (l_lca);
+	if (node == NULL)
+		return (0);
+	while (node->parent != NULL)
+	{
+		node = node->parent;
+		lv++;
+	}
+	return (lv);
 }
 
 /**
@@ -37,11 +28,26 @@ binary_tree_t *LCA(binary_tree_t *root,
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	binary_tree_t *root = (binary_tree_t *)first, *lca;
+	int lv_first, lv_second;
 
-	while (root->parent != NULL)
-		root = root->parent;
-	lca = LCA(root, (binary_tree_t *)first, (binary_tree_t *)second);
-	return (lca);
+	if (first == NULL || second == NULL)
+		return (NULL);
+	lv_first = level(first);
+	lv_second = level(second);
+	while (lv_first > lv_second)
+	{
+		first = first->parent;
+		lv_first--;
+	}
+	while (lv_second > lv_first)
+	{
+		second = second->parent;
+		lv_second--;
+	}
+	while (first != second)
+	{
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
 }
-
