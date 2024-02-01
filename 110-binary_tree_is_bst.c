85a546893cb7a12ac98480cf8b94f58002035dd7
youssef <youssefelghamour@gmail.com>
2024-02-01 03:58:49 -0800

    correct 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -15,11 +15,10 @@ int bst(const binary_tree_t *tree, int min, int max)
 	if (!tree)
 		return (1);
 
-	if ((tree->n < min) || (tree->n > max))
+	if ((tree->n <= min) || (tree->n >= max))
 		return (0);
 
-	return (bst(tree->left, min, tree->n - 1) &&
-			bst(tree->right, tree->n + 1, max));
+	return (bst(tree->left, min, tree->n) && bst(tree->right, tree->n, max));
 }
 
 /**
@@ -32,7 +31,7 @@ int bst(const binary_tree_t *tree, int min, int max)
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
+		return (0);
 
 	return (bst(tree, INT_MIN, INT_MAX));
 }
