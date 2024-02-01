7e4f6c29cfb5f6aac7119be0ab3df1885a4afa3a
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-01 15:33:51 -0800

    27. BST - Search

---
+++ 113-bst_search.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL || tree->n == value)
+		return ((bst_t *)tree);
+	if (tree->n > value)
+		return (bst_search(tree->left, value));
+	return (bst_search(tree->right, value));
+}
