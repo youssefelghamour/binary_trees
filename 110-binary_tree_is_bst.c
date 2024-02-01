aa3a11ed492f0c93765bf58d4089b355ad18c52d
youssef <youssefelghamour@gmail.com>
2024-02-01 03:16:34 -0800

    add 110-binary_tree_is_bst.c

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * bst - helper function to checks if a binary tree is a valid BST
+ *
+ * @tree: the root node of the tree to check
+ * @min: The minimum allowed value for nodes in the subtree
+ * @max: The minimum allowed value for nodes in the subtree
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int bst(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if ((tree->n <= min) || (tree->n >= max))
+		return (0);
+
+	return (bst(tree->left, min, tree->n) && bst(tree->left, tree->n, max));
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ *
+ * @tree: the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	return (bst(tree, INT_MIN, INT_MAX));
+}
