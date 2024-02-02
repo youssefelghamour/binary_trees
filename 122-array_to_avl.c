229301ebb55be4027cb9dc98d89dc139c3ec7d6c
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-01 18:14:13 -0800

    32. AVL - Array to AVL

---
+++ 122-array_to_avl.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_avl - builds an AVL tree from an array
+ * @array: array to represented in the AVL tree
+ * @size: size of the array
+ * Return: root node of the AVL tree
+ */
+avl_t *array_to_avl(int *array, size_t size)
+{
+	int i;
+	avl_t *tree = NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		avl_insert(&tree, array[i])
+	}
+	return (tree);
+}
